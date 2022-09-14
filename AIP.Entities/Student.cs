ba6fac21ab30b4eaba04f98dedbfc7e2a12e0b72
Jan Spanhel <spanhel@spanhel.eu>
2022-09-14 20:28:21 +0200

    Add project files.

---
+++ AIP.Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIP.Entities
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Account { get; set; }
+        public bool CollectionPayment { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ AIP.Entities/StudentClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIP.Entities
+{
+    internal class StudentClass
+    {
+        public int IdZak { get; set; }
+        public DateTime DateSince { get; set; }
+        public DateTime DateTo { get; set; }
+        public int IdSchoolClass { get; set; }
+        public string VarSymbol { get; set; }
+    }
+}
